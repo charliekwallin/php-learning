8cc1324d1bf3c64ee5abcb038cd4daa0f8797804
Charlie Wallin <charlie@charliewallin.com>
2020-05-26 15:17:04 -0400

    init commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+        </style>
+
+    </head>
+<body>
+<header>
+    
+<h1>
+
+    <?= "Hello" . htmlspecialchars($_GET['name']); ?>
+
+</h1>
+
+</header>
+
+</body>
+</html>
