f1e4f5e6fa6e1b7028bbd385da371a6b9a3d578f
Charlie Wallin <charlie@charliewallin.com>
2020-05-27 09:37:56 -0400

    Finished step05 separation of concerns

--- index.php
+++ index.php
@@ -1,29 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<?php
 
-    <style>
-        header {
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-        </style>
+$greeting = "Hello world.";
 
-    </head>
-<body>
-<header>
-    
-<h1>
-
-    <?= "Hello" . htmlspecialchars($_GET['name']); ?>
-
-</h1>
-
-</header>
-
-</body>
-</html>
+require 'index.view.php';
