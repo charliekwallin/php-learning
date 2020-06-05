2e3b10b93b7f65cc45d9ce268b32404d1e24a5a1
Charlie Wallin <charlie@charliewallin.com>
2020-06-05 07:13:15 -0400

    Finished lesson 12 on classes

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <?php
-require  'functions.php';
 
-$person = [
-    'age' => 55,
-    'hair' => 'Mostly gray',
-    'job' => 'Instructor in some capacity'
+class Task {
+    // class propertie
+    public $description;
+    public $completed = false;
+
+    // method
+    public function __construct($description)
+    {
+        $this->description = $description;
+    }
+
+    public function complete() 
+    {
+        $this->completed = true;
+    }
+
+    public function isComplete()
+    {
+        return $this->completed;
+    }
+
+}
+
+$task = new Task("Buy beer");
+
+$task-> isComplete();
+
+$tasks = [
+    new Task("Fill up bike tires"),
+    new Task("Get through pandemic"),
+    new Task("Practice concertina")
 ];
+
+$tasks[1]->complete();
+
 require 'index.view.php';
-
-old_enough(20);
-
-
-
-
--- index.view.php
+++ index.view.php
@@ -3,19 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Tasks</title>
 <body>
 
 <ul>
     
-    <?php foreach ($person as $feature => $value) : ?>
-        <li><strong><?= $feature; ?></strong>: <?=$value ?></li>
+    <?php foreach ($tasks as $task) : ?>
+        <li>
+            <?php if ($task->completed) : ?>
+                <strike> <?= $task->description; ?></strike>
+             <?php else : ?>           
+                <?= $task->description; ?>    
+            <?php endif; ?>
+        </li>
     <?php endforeach; ?>
-
-    
-
 </ul>
-
-
 </body>
 </html>
