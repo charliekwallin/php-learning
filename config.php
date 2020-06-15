5bf27fda5417e25543303cf177fd28d0c7f07712
Charlie Wallin <charlie@charliewallin.com>
2020-06-15 14:46:24 -0400

    Added gitignore and moved config info into it. config.php is now a template,  like it should be

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 return [
     'database' => [
-        'name' => 'charl989_mytodo',
-        'username' => 'charl989_doall',
-        'password' => 'Q1w2e3r4',
-        'connection' => 'mysql:host=127.0.0.1',
+        'name' => '',
+        'username' => '',
+        'password' => '',
+        'connection' => '',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]
