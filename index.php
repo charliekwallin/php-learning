9386237c255d3c1101257b3f7ee379fef1e32a1a
Charlie Wallin <charlie@charliewallin.com>
2020-06-15 16:45:45 -0400

    Modified core/bootstrap.php, index.php and created core/Request.php

--- index.php
+++ index.php
@@ -3,10 +3,5 @@
 // We need $query here because of the return value in the bootstrap.php file
 $query = require 'core/bootstrap.php';
 
-// $router = new Router;
-// require 'routes.php';
-
-  $uri = (trim($_SERVER['REQUEST_URI'],'/'));
-
-$router =  Router::load('routes.php');
-$router->direct($uri);
+require Router::load('routes.php')
+    ->direct(Request::uri());
