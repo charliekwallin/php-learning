b9462ace9b89b6c4509d91961d9744ff878aed28
Charlie Wallin <charlie@charliewallin.com>
2020-06-15 16:23:39 -0400

    Modified core/Router.php index.php routes.php

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 // We need $query here because of the return value in the bootstrap.php file
 $query = require 'core/bootstrap.php';
 
-$router = new Router;
-require 'routes.php';
+// $router = new Router;
+// require 'routes.php';
 
- $uri = (trim($_SERVER['REQUEST_URI'],'/'));
- require $router->direct($uri);
+  $uri = (trim($_SERVER['REQUEST_URI'],'/'));
+
+$router =  Router::load('routes.php');
+$router->direct($uri);
--- routes.php
+++ routes.php
@@ -3,7 +3,7 @@
 $router->define([
     'todos' => 'controllers/index.php',
     'todos/about' => 'controllers/about.php',
-    'todos/about-culture' => 'controllers/about-culture.php',
+    'todos/about/culture' => 'controllers/about-culture.php',
     'todos/contact' => 'controllers/contact.php'
 ]);
 
