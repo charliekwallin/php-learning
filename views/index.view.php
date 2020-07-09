5bb4d02b1f609d9bd49706e50af898741482d9e8
Charlie Wallin <charlie@charliewallin.com>
2020-07-09 10:01:14 -0400

    Finished drying up views however about/culture is not recognizing the stylesheet. I need to move on. I will figure this out later.

--- views/index.view.php
+++ views/index.view.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tasks</title>
-<body>
+<?php require('partials/head.php'); ?>
+
+<h1>Tasks</h1>
 
 <ul>
     
@@ -18,5 +14,5 @@
         </li>
     <?php endforeach; ?>
 </ul>
-</body>
-</html>
+
+<?php require('partials/foot.php'); ?>
