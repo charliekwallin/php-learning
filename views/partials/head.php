1b028265c0df8e5d95efb7227138dba287ef9ee5
Charlie Wallin <charlie@charliewallin.com>
2020-07-10 13:29:29 -0400

    Fixed views/partials/head.php. Now the styles work on all pages.

--- views/partials/head.php
+++ views/partials/head.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="public/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="/public/css/styles.css">
     <title>About</title>
 </head>
 <body>
-    <?php require('views/partials/nav.php'); ?>
+    <?php require('nav.php'); ?>
